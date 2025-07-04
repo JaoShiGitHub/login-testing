e5c3f4ecb624bdcb7b8b7d91dab4f7eb7242a62b
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-07-04 20:22:12 +0700

    Prevent case: user_data === undifined

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,13 +4,23 @@ const get_user = async (req, res) => {
   const { customer_id } = req.query;
 
   try {
-    const user_info = await pool.query("SELECT * FROM users WHERE id = $1", [
+    const user = await pool.query("SELECT * FROM users WHERE id = $1", [
       customer_id,
     ]);
-    return res.status(200).json({
-      message: "User fetched successfully",
-      user_data: user_info.rows[0],
-    });
+
+    const user_data = user.rows[0];
+
+    if (user_data === undefined) {
+      return res.status(404).json({
+        status: "error",
+        message: `Not found user id: ${customer_id}`,
+      });
+    } else {
+      return res.status(200).json({
+        message: "User fetched successfully",
+        user_data: user_data,
+      });
+    }
   } catch (error) {
     console.log("Error: ", error.message);
     return res.status(500).json({
