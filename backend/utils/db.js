344fe6fb7fc9a0a46883b5ae7815db0f1b6ee344
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-07-10 15:43:36 +0700

    Change password

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,7 @@ import * as pg from "pg";
 const { Pool } = pg.default;
 
 const pool = new Pool({
-  connectionString: "postgresql://postgres:081400@localhost:5432/login_testing",
+  connectionString: "postgresql://postgres:000000@localhost:5432/login_testing",
 });
 
 export { pool };
