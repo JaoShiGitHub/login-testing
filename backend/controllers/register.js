032d0419279d202238800fbf921c1d39ad4a67ac
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-07-11 20:40:28 +0700

    Test insert img

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -1,4 +1,5 @@
 import { pool } from "../utils/db.js";
+import { readFile } from "fs/promises";
 import bcrypt from "bcrypt";
 
 const register = async (req, res) => {
@@ -8,10 +9,12 @@ const register = async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const password = await bcrypt.hash(user.password, salt);
 
+  const imageBuffer = await readFile("../lili2.jpg");
+
   try {
     await pool.query(
-      `INSERT INTO users (username, email, password, status) VALUES ($1, $2, $3, $4)`,
-      [username, email, password, status]
+      `INSERT INTO users (username, email, password, status, photo) VALUES ($1, $2, $3, $4, $5)`,
+      [username, email, password, status, imageBuffer]
     );
     return res.status(200).json({ message: "User registered successfully" });
   } catch (error) {
