bbb8c85b7e366c17696dc9db79bf130f4b06689c
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-07-12 23:07:13 +0700

    Update form edit

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -5,6 +5,8 @@ import { useTranslation, Trans } from "react-i18next";
 import LangButtons from "./LangButtons";
 
 const items_center = "flex flex-col items-center";
+const css_button =
+  "hover:font-bold hover:text-white hover:bg-gray-950 px-2 border-2 rounded-md";
 
 function Profile() {
   const { logout, user } = useAuth();
@@ -14,6 +16,7 @@ function Profile() {
   const [userId, setUserId] = useState(null);
   const [userData, setUserData] = useState(null);
   const [imageUrl, setImageUrl] = useState("");
+  const [image, setImage] = useState(null);
   const [newUserInfo, setNewUserInfo] = useState({
     username: "",
     email: "",
@@ -65,52 +68,82 @@ function Profile() {
     }
   };
 
+  const handleImageChange = (e) => {
+    setImage(e.target.files[0]);
+  };
+
   const handleLogout = async () => {
     await logout();
   };
 
   return (
     <div className={`${items_center} h-screen`}>
-      {/* Text Greeting */}
       <div className="mt-10">
         <LangButtons />
       </div>
+      {/* Text Greeting */}
       <h1 className="font-bold text-4xl mb-10 mt-20">
         <Trans i18nKey="welcome">Welcome</Trans> {userData?.username} {":)"}
       </h1>
-      {/* <div className="w-[200px] h-[200px] bg-pink-200"></div> */}
-      <img
-        src={imageUrl}
-        alt={userData?.username}
-        className="w-[200px] h-[200px]"
-      />
-      <section className={`${items_center} gap-y-2 mt-8 mb-20`}>
-        <p>
-          <b>
-            <Trans i18nKey="username">Username</Trans>
-          </b>
-          : {userData?.username}
-        </p>
-        <p>
-          <b>
-            <Trans i18nKey="email">Email</Trans>
-          </b>
-          : {userData?.email}
-        </p>
-        <p>
-          <b>
-            <Trans i18nKey="status">Status</Trans>
-          </b>
-          : {userData?.status}
-        </p>
+      {/* Edit Profile */}
+      {editFormSwitch ? (
+        <form className={items_center} onSubmit={handleOnSubmit}>
+          <input
+            type="file"
+            name="photo"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
+        </form>
+      ) : (
+        <div className={items_center}>
+          <img
+            src={imageUrl}
+            alt={userData?.username}
+            className="w-[200px] h-[200px]"
+          />
+          <section className={`${items_center} gap-y-2 mt-8 mb-20`}>
+            <p>
+              <b>
+                <Trans i18nKey="username">Username</Trans>
+              </b>
+              : {userData?.username}
+            </p>
+            <p>
+              <b>
+                <Trans i18nKey="email">Email</Trans>
+              </b>
+              : {userData?.email}
+            </p>
+            <p>
+              <b>
+                <Trans i18nKey="status">Status</Trans>
+              </b>
+              : {userData?.status}
+            </p>
+          </section>
+        </div>
+      )}
+      <section className="flex gap-x-8">
+        <button className={css_button} onClick={handleLogout}>
+          <Trans i18nKey="logout">Log out</Trans>
+        </button>
+        {editFormSwitch ? (
+          <button
+            className={css_button}
+            onClick={() => setEditFormSwitch(false)}
+          >
+            Cancel
+          </button>
+        ) : (
+          <button
+            className={css_button}
+            onClick={() => setEditFormSwitch(true)}
+          >
+            Edit
+          </button>
+        )}
       </section>
-      <p></p>
-      <button
-        className="hover:font-bold hover:text-white hover:bg-gray-950 px-2 border-2 rounded-md"
-        onClick={handleLogout}
-      >
-        <Trans i18nKey="logout">Log out</Trans>
-      </button>
     </div>
   );
 }
