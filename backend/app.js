5c66bff7ebe4c6fe9b053f9b4749d47ae9aaab4a
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-07-13 19:19:30 +0700

    Update Backend

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const port = 4000;
 dotenv.config();
 
 app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+// app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use(cookieParser());
 app.use(
