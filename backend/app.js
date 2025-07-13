43695d1aaab3dec523967744931771f369341d71
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-07-13 18:27:35 +0700

    Fix Payload Too Large issues

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,9 @@ const app = express();
 const port = 4000;
 dotenv.config();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 app.use(cookieParser());
 app.use(
   cors({
