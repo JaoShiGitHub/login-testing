b959b5c76522fa2b5d3ebd469eaab077525158a0
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-07-16 15:29:30 +0700

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -30,12 +30,15 @@ function Profile() {
   const [previewImage, setPreviewImage] = useState(null);
   const [newImage, setNewImage] = useState(null);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     get_userData();
   }, []);
 
   const get_userData = async (id) => {
     try {
+      setIsLoading(true);
       const response = await axios.get(`http://localhost:4000/profile`, {
         withCredentials: true,
       });
@@ -57,7 +60,7 @@ function Profile() {
       setUsername(user?.username);
       setEmail(user?.email);
       setStatus(user?.status);
-
+      setIsLoading(false);
       console.log("User data fetched successfully:", user);
     } catch (error) {
       console.error("Error fetching user data");
@@ -139,175 +142,189 @@ function Profile() {
   };
 
   return (
-    <div className={`${items_center} h-screen`}>
-      <div className="mt-10">
-        <LangButtons />
-      </div>
-      {isAccountDeleted ? (
-        <strong className="text-red-700 text-4xl  mb-10 mt-20">
-          {userData?.username} has been deleted. 🥺
-        </strong>
+    <div>
+      {" "}
+      {isLoading ? (
+        <p>Loading....</p>
       ) : (
-        <h1 className="font-bold text-4xl mb-10 mt-20">
-          <Trans i18nKey="welcome">Welcome</Trans> {userData?.username} {":)"}
-        </h1>
-      )}
+        <div className={`${items_center} h-screen`}>
+          <div className="mt-10">
+            <LangButtons />
+          </div>
 
-      {/* Edit Profile Condition: "editFormSwitch = true ? open form : show profile" */}
-      {editFormSwitch ? (
-        <form
-          id="form-edit"
-          className={`${items_center} gap-5 mb-10`}
-          onSubmit={handleOnSubmitFormEdit}
-        >
-          <img
-            src={previewImage || imageUrl}
-            alt={userData?.username}
-            className="w-[200px] h-[200px]"
-          />
-          <label className="bg-blue-500 hover:bg-blue-900 mb-5 text-white px-4 py-1 rounded cursor-pointer inline-block">
-            <Trans i18nKey="changeImage">Change Image</Trans>
-            <input
-              type="file"
-              name="photo"
-              accept="image/*"
-              onChange={handleImageChange}
-              className="hidden"
-            />
-          </label>
-          <label>
-            <b>
-              <Trans i18nKey="username">Username</Trans>:
-            </b>
-            <input
-              className="mx-4 px-2 py-1 rounded border"
-              type="text"
-              placeholder={capitalizeFirstLetter("username")}
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </label>
-          <label>
-            <b>
-              <Trans i18nKey="email">Email</Trans>:
-            </b>
-            <input
-              className="mx-4 px-2 py-1 rounded border"
-              type="text"
-              placeholder={capitalizeFirstLetter("email")}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
+          {isAccountDeleted ? (
+            <strong className="text-red-700 text-4xl  mb-10 mt-20">
+              {userData?.username} has been deleted. 🥺
+            </strong>
+          ) : (
+            <h1 className="font-bold text-4xl mb-10 mt-20">
+              <Trans i18nKey="welcome">Welcome</Trans> {userData?.username}{" "}
+              {":)"}
+            </h1>
+          )}
 
-          <label>
-            <b>
-              <Trans i18nKey="status">Status</Trans>:
-            </b>
-            <input
-              className="mx-4 px-2 py-1 rounded border"
-              type="text"
-              placeholder={capitalizeFirstLetter("status")}
-              value={status}
-              onChange={(e) => setStatus(e.target.value)}
-            />
-          </label>
-        </form>
-      ) : (
-        <div className={items_center}>
-          <img
-            src={imageUrl}
-            alt={userData?.username}
-            className="w-[200px] h-[200px]"
-          />
-          <section className={`${items_center} gap-y-2 mt-8 mb-20`}>
-            <p>
-              <b>
-                <Trans i18nKey="username">Username</Trans>
-              </b>
-              : {userData?.username}
-            </p>
-            <p>
-              <b>
-                <Trans i18nKey="email">Email</Trans>
-              </b>
-              : {userData?.email}
-            </p>
-            <p>
-              <b>
-                <Trans i18nKey="status">Status</Trans>
-              </b>
-              : {userData?.status}
-            </p>
-          </section>
-        </div>
-      )}
-
-      {isAccountDeleted ? (
-        <p className="text-lg">
-          Go back to{"  "}
-          <button
-            className="text-blue-500 font-bold ml-1"
-            onClick={handleFinishDeleteAcc}
-          >
-            Login
-          </button>
-        </p>
-      ) : (
-        <section className="flex flex-col gap-y-16 gap-x-8 mb-10">
+          {/* Edit Profile Condition: "editFormSwitch = true ? open form : show profile" */}
           {editFormSwitch ? (
-            <div className="flex gap-4">
-              <button className={css_button} onClick={handleOnClickCancel}>
-                <Trans i18nKey="cancel">Cancel</Trans>
-              </button>
-
-              <button className={css_button} type="submit" form="form-edit">
-                <Trans i18nKey="save">Save</Trans>
-              </button>
-            </div>
-          ) : (
-            <button
-              className={`${css_button} self-center`}
-              onClick={() => setEditFormSwitch(true)}
+            <form
+              id="form-edit"
+              className={`${items_center} gap-5 mb-10`}
+              onSubmit={handleOnSubmitFormEdit}
             >
-              <Trans i18nKey="edit">Edit</Trans>
-            </button>
+              <img
+                src={previewImage || imageUrl}
+                alt={userData?.username}
+                className="w-[200px] h-[200px]"
+              />
+              <label className="bg-blue-500 hover:bg-blue-900 mb-5 text-white px-4 py-1 rounded cursor-pointer inline-block">
+                <Trans i18nKey="changeImage">Change Image</Trans>
+                <input
+                  type="file"
+                  name="photo"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  className="hidden"
+                />
+              </label>
+              <label>
+                <b>
+                  <Trans i18nKey="username">Username</Trans>:
+                </b>
+                <input
+                  className="mx-4 px-2 py-1 rounded border"
+                  type="text"
+                  placeholder={capitalizeFirstLetter("username")}
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </label>
+              <label>
+                <b>
+                  <Trans i18nKey="email">Email</Trans>:
+                </b>
+                <input
+                  className="mx-4 px-2 py-1 rounded border"
+                  type="text"
+                  placeholder={capitalizeFirstLetter("email")}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+
+              <label>
+                <b>
+                  <Trans i18nKey="status">Status</Trans>:
+                </b>
+                <input
+                  className="mx-4 px-2 py-1 rounded border"
+                  type="text"
+                  placeholder={capitalizeFirstLetter("status")}
+                  value={status}
+                  onChange={(e) => setStatus(e.target.value)}
+                />
+              </label>
+            </form>
+          ) : (
+            <div className={items_center}>
+              <img
+                src={imageUrl}
+                alt={userData?.username}
+                className="w-[200px] h-[200px]"
+              />
+              <section className={`${items_center} gap-y-2 mt-8 mb-20`}>
+                <p>
+                  <b>
+                    <Trans i18nKey="username">Username</Trans>
+                  </b>
+                  : {userData?.username}
+                </p>
+                <p>
+                  <b>
+                    <Trans i18nKey="email">Email</Trans>
+                  </b>
+                  : {userData?.email}
+                </p>
+                <p>
+                  <b>
+                    <Trans i18nKey="status">Status</Trans>
+                  </b>
+                  : {userData?.status}
+                </p>
+              </section>
+            </div>
           )}
 
-          {deleteAccount ? (
-            <section>
-              <Trans i18nKey="deleteAccount">
-                Do you really want to eliminate this account? 🥺
-              </Trans>
-              <div className="mt-5 flex gap-x-10 justify-center">
-                <button className={css_button} onClick={handleDeleteAccount}>
-                  <Trans i18nKey="yes">Yes</Trans>
-                </button>
-                <button
-                  className={css_button}
-                  onClick={() => setDeleteAccount(false)}
-                >
-                  <Trans i18nKey="no">No</Trans>
-                </button>
-              </div>
-            </section>
+          {isAccountDeleted ? (
+            <p className="text-lg">
+              Go back to{"  "}
+              <button
+                className="text-blue-500 font-bold ml-1"
+                onClick={handleFinishDeleteAcc}
+              >
+                Login
+              </button>
+            </p>
           ) : (
-            <section>
-              <button
-                className={`${css_button} mr-5 w-fit`}
-                onClick={handleLogout}
-              >
-                <Trans i18nKey="logout">Log out</Trans>
-              </button>
-              <button
-                className={css_button}
-                onClick={() => setDeleteAccount(true)}
-              >
-                <Trans i18nKey="eliminateAccount">Eliminate this account</Trans>
-              </button>
+            <section className="flex flex-col gap-y-16 gap-x-8 mb-10">
+              {editFormSwitch ? (
+                <div className="flex gap-4">
+                  <button className={css_button} onClick={handleOnClickCancel}>
+                    <Trans i18nKey="cancel">Cancel</Trans>
+                  </button>
+
+                  <button className={css_button} type="submit" form="form-edit">
+                    <Trans i18nKey="save">Save</Trans>
+                  </button>
+                </div>
+              ) : (
+                <button
+                  className={`${css_button} self-center`}
+                  onClick={() => setEditFormSwitch(true)}
+                >
+                  <Trans i18nKey="edit">Edit</Trans>
+                </button>
+              )}
+
+              {deleteAccount ? (
+                <section>
+                  <Trans i18nKey="deleteAccount">
+                    Do you really want to eliminate this account? 🥺
+                  </Trans>
+                  <div className="mt-5 flex gap-x-10 justify-center">
+                    <button
+                      className={css_button}
+                      onClick={handleDeleteAccount}
+                    >
+                      <Trans i18nKey="yes">Yes</Trans>
+                    </button>
+                    <button
+                      className={css_button}
+                      onClick={() => setDeleteAccount(false)}
+                    >
+                      <Trans i18nKey="no">No</Trans>
+                    </button>
+                  </div>
+                </section>
+              ) : (
+                <section>
+                  <button
+                    className={`${css_button} mr-5 w-fit`}
+                    onClick={handleLogout}
+                  >
+                    <Trans i18nKey="logout">Log out</Trans>
+                  </button>
+                  <button
+                    className={css_button}
+                    onClick={() => setDeleteAccount(true)}
+                  >
+                    <Trans i18nKey="eliminateAccount">
+                      Eliminate this account
+                    </Trans>
+                  </button>
+                </section>
+              )}
             </section>
           )}
-        </section>
+        </div>
       )}
     </div>
   );
