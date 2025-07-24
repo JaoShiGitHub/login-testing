f4a815c7eb1059cca105e979b21ee90cbb197bc6
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2025-07-24 17:50:23 +0700

    Delete backend/utils directory

--- backend/utils/db.js
+++
@@ -1,8 +0,0 @@
-import * as pg from "pg";
-const { Pool } = pg.default;
-
-const pool = new Pool({
-  connectionString: "postgresql://postgres:081400@localhost:5432/login_testing",
-});
-
-export { pool };
